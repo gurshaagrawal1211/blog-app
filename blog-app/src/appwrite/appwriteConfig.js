5fc712951d40b930ddbb314572e1efefec838cf2
gurshaagrawal1211 <gurshaagrawal@gmail.com>
2023-11-23 22:23:58 +0530

    commit

---
+++ blog-app/src/appwrite/appwriteConfig.js
@@ -0,0 +1,86 @@
+import config from "../config/config";
+
+import {Client, Account, ID, Databases, Storage, Query} from "appwrite"
+
+export class Service{
+    client = new Client() 
+    databases
+    storage
+
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId)
+        this.databases = new Databases(this.client)
+        this.storage = new Storage(this.client)
+    }
+
+    async createPost({title, slug, content, featuredImage, status, userId}){
+        try {
+            return await this.databases.createDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug, //id
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            )
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async updatePost(slug,{title, content, featuredImage, status})
+    {
+        try {
+            return await this.databases.updateDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+            )
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            )
+            return true
+        } catch (error) {
+            throw error
+            return false
+        }
+    }
+
+    async getPost(slug){
+        try {
+            return await this.databases(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            throw error
+            return false
+        }
+    }
+}
+
+const service = new Service()
+
+export default service
