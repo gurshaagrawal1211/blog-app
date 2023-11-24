ac006604939f74300f12ee66ca3460ad05378ebd
gurshaagrawal1211 <gurshaagrawal@gmail.com>
2023-11-24 23:40:14 +0530

    commit

--- blog-app/src/App.jsx
+++ blog-app/src/App.jsx
@@ -1,10 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import {useDispatch} from "react-redux"
+import authService from "./appwrite/auth"
+import { login,logout } from './store/authSlice'
 import './App.css'
+import {Header, Footer} from "./components"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true)
+
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  },
+  [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
 
   return (
     <>
