2c9f23bd31270ae1f6937f95854fe9f655a9488e
gurshaagrawal1211 <gurshaagrawal@gmail.com>
2023-11-26 10:38:59 +0530

    commit

--- blog-app/src/components/Header/Header.jsx
+++ blog-app/src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Container, Logo, LogoutBtn } from '../index'
 import { Link, useNavigate } from 'react-router-dom'
-import { UseSelector, useSelector } from 'react-redux/es/hooks/useSelector'
+import {useSelector} from 'react-redux'
 
 const Header = () => {
 
